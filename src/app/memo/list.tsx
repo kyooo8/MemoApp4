5af1c26567f19c893a4521d0533721e9ae14dd19
kyooo8 <kyosuke.7876@gmail.com>
2024-08-10 04:42:42 +0900

    memoList

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,18 +1,22 @@
 import { View, StyleSheet } from "react-native";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
 
 import MemoListItem from "../../components/MemoListItem";
 import CircleButton from "../../components/CircleButton";
 import Icon from "../../components/Icon";
 import LogOutButton from "../../components/LogOutButton";
+import { type Memo } from "../../../types/memo";
 
 import { router, useNavigation } from "expo-router";
+import { db, auth } from "../../config";
 
 const handlePress = (): void => {
   router.push("/memo/create");
 };
 
 const List = (): JSX.Element => {
+  const [memos, setMemos] = useState<Memo[]>([]);
   const navigation = useNavigation();
   useEffect(() => {
     navigation.setOptions({
@@ -22,12 +26,34 @@ const List = (): JSX.Element => {
     });
   }, []);
 
+  useEffect(() => {
+    if (auth.currentUser === null) {
+      return;
+    }
+    const ref = collection(db, `users/${auth.currentUser.uid}/memos`);
+    const q = query(ref, orderBy("updatedAt", "desc"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const remoteMemos: Memo[] = [];
+      snapshot.forEach((doc) => {
+        console.log("memo", doc.data());
+        const { bodyText, updatedAt } = doc.data();
+        remoteMemos.push({
+          id: doc.id,
+          bodyText,
+          updatedAt,
+        });
+      });
+      setMemos(remoteMemos);
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <View style={styles.container}>
       <View>
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
+        {memos.map((memo) => (
+          <MemoListItem memo={memo} />
+        ))}
       </View>
       <CircleButton onPress={handlePress}>
         <Icon name="plus" size={40} color="#ffffff" />
--- src/components/MemoListItem.tsx
+++ src/components/MemoListItem.tsx
@@ -3,14 +3,27 @@ import { Link } from "expo-router";
 import React from "react";
 
 import Icon from "./Icon";
+import { type Memo } from "../../types/memo";
 
-const MemoListItem = (): JSX.Element => {
+interface Props {
+  memo: Memo;
+}
+
+const MemoListItem = (props: Props): JSX.Element | null => {
+  const { memo } = props;
+  const { bodyText, updatedAt } = memo;
+  if (bodyText === null || updatedAt === null) {
+    return null;
+  }
+  const dateString = updatedAt.toDate().toLocaleString("ja-JP");
   return (
     <Link href={"/memo/detail"} asChild>
       <TouchableOpacity style={styles.memoListItem}>
         <View>
-          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-          <Text style={styles.memoListItemDate}>2023年10月1日　10:00</Text>
+          <Text numberOfLines={1} style={styles.memoListItemTitle}>
+            {bodyText}
+          </Text>
+          <Text style={styles.memoListItemDate}>{dateString}</Text>
         </View>
         <TouchableOpacity>
           <Icon name="delete" size={32} color="#b0b0b0" />
@@ -36,6 +49,7 @@ const styles = StyleSheet.create({
   memoListItemTitle: {
     fontSize: 16,
     lineHeight: 32,
+    fontWeight: "bold",
   },
   memoListItemDate: {
     fontSize: 12,
